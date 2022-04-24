be7eb1193abc57b93158a55baadcd85908df6bdd
Taner-Y-Banth <62570956+Taner-Y-Banth@users.noreply.github.com>
2022-04-23 18:38:30 -0700

    unique connections for  clients

--- coral-detect/index.js
+++ coral-detect/index.js
@@ -1,5 +1,4 @@
 import fsPromises from 'fs/promises';
-import Jimp from 'jimp';
 import minimist from 'minimist';
 import { NstrumentaClient } from 'nstrumenta';
 import ws from 'ws';
@@ -13,17 +12,18 @@ nstClient.addListener("open", () => {
 
     console.log("websocket opened successfully");
 
-    nstClient.addSubscription('preprocessing', async (buff) => {
+    nstClient.addSubscription('preprocessing', async (msg) => {
         try {
-            const filename = `${Date.now()}.jpeg`
+            const buff = Uint8Array.from(atob(msg.data), (c) => c.charCodeAt(0))
+            const filename = `${msg.responseChannel}${Date.now()}.jpeg`
             await fsPromises.writeFile(filename, buff);
 
-                const { stdout } = await $`python3 detect_image.py -m ./test_data/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite -l ./test_data/coco_labels.txt -i ${filename}`
-                await fsPromises.rm(filename)
-                nstClient.send(
-                    'visionText',
-                    stdout
-                );
+            const { stdout } = await $`python3 detect_image.py -m ./test_data/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite -l ./test_data/coco_labels.txt -i ${filename}`
+            await fsPromises.rm(filename)
+            nstClient.send(
+                msg.responseChannel,
+                stdout
+            );
         } catch (err) {
             console.error(err)
         };
