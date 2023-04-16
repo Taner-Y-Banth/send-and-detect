edd48b9c043431f15b54dca452837c65c14b40a9
nst-t <tyler@nstrumenta.com>
2023-04-16 14:14:26 -0700

    fixes FS writeFile ; extracts frames every second

--- frame-extractor/index.ts
+++ frame-extractor/index.ts
@@ -1,7 +1,8 @@
 import minimist from "minimist";
 import { NstrumentaClient } from "nstrumenta";
-import { readFile } from "fs/promises";
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import { readFile, writeFile, mkdir } from "fs/promises";
+import path from 'path'
 
 const argv = minimist(process.argv.slice(2));
 const wsUrl: string = argv.wsUrl;
@@ -9,11 +10,47 @@ const input: string = argv.input;
 const nstClient = new NstrumentaClient();
 
 const loadInput = async (input: string) => {
-  const ffmpeg = createFFmpeg({ log: true });
+  let duration = 0;
+  const ffmpeg = createFFmpeg({
+    log: true,
+  });
+  //parse the logger messages to extract the duration
+  ffmpeg.setLogger((message) => {
+    console.log('logger!', message)
+    const regex = /Duration: (\d{2})\:(\d{2})\:(\d{2})\.(\d{2})/gm;
+    const matches = regex.exec(String(message.message));
+    if (matches != null) {
+      const h = Number(matches[1]);
+      const m = Number(matches[2]);
+      const s = Number(matches[3]);
+      // const ms = Number(matches[4]);
+      duration = h * 3600 + m * 60 + s;
+    }
+  },)
+
   await ffmpeg.load();
-  ffmpeg.FS("writeFile", input, await fetchFile(input));
-  await ffmpeg.run("-i", input);
-};
+
+  console.log(input);
+  const { name, dir, base: infileName } = path.parse(input);
+  console.log(`extracting frames from ${infileName}`)
+
+  ffmpeg.FS("writeFile", infileName, await fetchFile(input));
+  //run once just to get duration
+  await ffmpeg.run("-i", infileName);
+  console.log({ duration });
+  mkdir('out', { recursive: true });
+  for (let ss = 0; ss < duration; ss += 1) {
+    const ssDisplayNumber = `${ss*1000}`.padStart(9, '0');
+    const outFileName = `./out/outframe${ssDisplayNumber}.jpeg`
+    await ffmpeg.run("-ss", `${ss}`, "-i", infileName, "outframe.jpeg");
+    await writeFile(outFileName, ffmpeg.FS("readFile", "outframe.jpeg"));
+  
+    //TODO:
+    // here you can use the image in outFileName to call vision api
+
+
+  };
+}
 loadInput(input);
 
 nstClient.addListener("open", async () => {
